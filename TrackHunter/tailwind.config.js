2dfbd5be2c37f2dbbefcedee6b86a531899af582
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-05-09 09:41:52 +0200

    Import TYPOGRAPHIES

--- TrackHunter/tailwind.config.js
+++ TrackHunter/tailwind.config.js
@@ -2,22 +2,27 @@
 module.exports = {
   content: [],
   theme: {
-    'black': '#090A0E',
-    'black-75': '#101118',
-    'black-10': '#1011181A',
+    colors: {
+      'black': '#090A0E',
+      'black-75': '#101118',
+      'black-10': '#1011181A',
 
-    'white': '#FFFFFF',
-    'white-75': '#FFFFFFBF',
-    'white-50': '#FFFFFF80',
-    'white-10': '#FFFFFF1A',
+      'white': '#FFFFFF',
+      'white-75': '#FFFFFFBF',
+      'white-50': '#FFFFFF80',
+      'white-10': '#FFFFFF1A',
 
-    'green': '#2BB299',
-    'red': '#EA5A5F',
+      'green': '#2BB299',
+      'red': '#EA5A5F',
 
-    'blue': '#3B74F7',
-    'blue-50': '#3B74F780',
-    'blue-25': '#3B74F740',
-    extend: {},
+      'blue': '#3B74F7',
+      'blue-50': '#3B74F780',
+      'blue-25': '#3B74F740',
+    },
+    fontFamily: {
+      'Anton': ['Anton', 'sans-serif'],
+      'Ubuntu': ['Ubuntu', 'sans-serif'],
+    },
   },
   plugins: [],
 }
