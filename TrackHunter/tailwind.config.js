c69c4faec625eb66dff59819eae447c654d177e0
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-05-09 09:36:46 +0200

    Import COULEURS manquantes

--- TrackHunter/tailwind.config.js
+++ TrackHunter/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     'black': '#090A0E',
     'black-75': '#101118',
+    'black-10': '#1011181A',
 
     'white': '#FFFFFF',
     'white-75': '#FFFFFFBF',
@@ -12,8 +13,9 @@ module.exports = {
 
     'green': '#2BB299',
     'red': '#EA5A5F',
-    
+
     'blue': '#3B74F7',
+    'blue-50': '#3B74F780',
     'blue-25': '#3B74F740',
     extend: {},
   },
