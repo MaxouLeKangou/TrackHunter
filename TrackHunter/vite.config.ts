6cac17c876888f4905dcdfe6097f0a96b909d5f2
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-05-09 10:42:04 +0200

    Config VITE-PLUGIN-PAGES

--- TrackHunter/vite.config.ts
+++ TrackHunter/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Pages from 'vite-plugin-pages'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
