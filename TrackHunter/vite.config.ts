904284fddde125b9e6af0c3ba8843cf179f937bf
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-05-09 11:49:46 +0200

    Fix BUG - vite-plugin-pages

--- TrackHunter/vite.config.ts
+++ TrackHunter/vite.config.ts
@@ -7,7 +7,7 @@ import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Pages()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
